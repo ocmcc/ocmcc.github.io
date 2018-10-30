1082d16a5e943569adf6715583323e7984acb5d3
ocmcc <wjy748971253@163.com>
2018-10-30 18:04:55 +0800

    Site updated: 2018-10-30 18:04:50

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,29 @@
 /**
  * Created by Medi on 2018/10/5.
  */
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  autoplay: false,
+  mini: false,
+  autoplay: false,
+  theme: '#FADFA3',
+  loop: 'all',
+  volume: 0.7,
+  listFolded: true,
+  listMaxHeight: 90,
+  fixed: false,
+  audio: [
+    {
+      name: '걱정말아요 그대(你不要担心)',
+      artist: '野菊花',
+      url: 'http://www.ytmp3.cn/down/51124.mp3',
+      cover: ''
+    },
+    {
+      name: '莫名我就喜欢你',
+      artist: '',
+      url: 'http://www.ytmp3.cn/down/35709.mp3',
+      cover: ''
+    }
+  ]
+});
