9c76e7a8aadd85e544603367dd74bdd273b18598
ocmcc <wjy748971253@163.com>
2018-11-02 10:17:03 +0800

    Site updated: 2018-11-02 10:16:59

--- dist/music.js
+++ dist/music.js
@@ -17,13 +17,19 @@ const ap = new APlayer({
       name: '걱정말아요 그대(你不要担心)',
       artist: '野菊花',
       url: 'http://www.ytmp3.cn/down/51124.mp3',
-      cover: ''
+      cover: 'http://p1.music.126.net/FhaoHC8Gp9m0FLKvcWuHHQ==/6657542906300239.jpg?param=130y130'
     },
     {
-      name: '莫名我就喜欢你',
-      artist: '',
-      url: 'http://www.ytmp3.cn/down/35709.mp3',
-      cover: ''
-    }
+      name: '10 Lovers',
+      artist: 'The Black Keys',
+      url: 'http://link.hhtjim.com/163/28497099.mp3',
+      cover: 'http://p2.music.126.net/Ah1qSSCas4UbybIWKo5aCQ==/5868093557600822.jpg?param=130y130'
+    },
+    {
+      name: "What's Up",
+      artist: '4 Non Blondes',
+      url: 'http://link.hhtjim.com/163/2112908.mp3',
+      cover: 'http://p2.music.126.net/6MAIdskLwJitnr-thO96Mw==/2259496395099002.jpg?param=130y130'
+    },
   ]
 });
